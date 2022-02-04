8684400c647f3e07329cc8f18bdf50a2abb8cde4
graham <grey.wolf.coat@gmail.com>
2022-02-04 16:49:06 +0000

    updated script.sh and added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Week 2 Lab – Bash & Git
+Graham O'Mahony 21121192
+[artofgraham](https://github.com/artofgraham/devops-lab-2-task)
+---
+To run the script.sh run: 'bash script.sh'
--- script.sh
+++ script.sh
@@ -7,3 +7,11 @@ touch 1_1.txt 1_2.txt 1_3.txt
 cd ..
 cd fol_2
 touch 2_1.txt 2_2.txt 2_3.txt
+cd ..
+cd fol_1
+chmod 600 *_1.txt *_3.txt
+chmod 777 *_2.txt
+cd ..
+cd fol_2
+chmod 600 *_1.txt *_3.txt
+chmod 777 *_2.txt
